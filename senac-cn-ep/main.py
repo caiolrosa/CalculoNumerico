85a08be2b4d88bb1827dff9c1cd0d9f2b7f22790
Caio Rosa <caiolrosa1912@gmail.com>
2018-06-10 23:41:56 -0300

    Python -> fortran connection, cleanup fortran code

--- senac-cn-ep/main.py
+++ senac-cn-ep/main.py
@@ -6,5 +6,4 @@ if __name__ == "__main__":
         strArr = f.read().split("\n")
         input = [i.split(" ") for i in strArr]
         input = [[float(j) for j in i] for i in input[:len(input) - 1]]
-        a = [0]*len(input)
-        print(r.get_ranking(input, a, 0))
+        r.get_ranking(input)
--- senac-cn-ep/ranking.f90
+++ senac-cn-ep/ranking.f90
@@ -1,38 +1,46 @@
-SUBROUTINE get_ranking(matrix, ranking, statusCode)
-	INTEGER :: statusCode, matrixSize, i
-	DOUBLE PRECISION :: matrixSizeDBL, threshold, temp
+SUBROUTINE get_ranking(matrix)
+	INTEGER :: matrixSize, i
+	DOUBLE PRECISION :: matrixSizeDBL, threshold, temp, m
 	DOUBLE PRECISION, DIMENSION(:,:) :: matrix
-	DOUBLE PRECISION, DIMENSION(:,:) :: ranking
-	DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: S
+	DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: S, ranking
 	
 	statusCode = 123
 	threshold = 1E-5
+	m = 0.15
 
 	! Verifica se a matriz é quadrada
 	matrixSizeDBL = SQRT(REAL(SIZE(matrix)))
 	matrixSize = INT(matrixSizeDBL)
 	IF ((matrixSizeDBL * matrixSizeDBL) /= (INT(matrixSizeDBL) * INT(matrixSizeDBL))) THEN
-		statusCode = 1
+		PRINT *, "ERRO: Matriz de entrada não é quadrada"
 		STOP 1
 	END IF 
 
-	allocate (S(matrixSize, 1))
+	ALLOCATE(S(matrixSize, 1))
+	ALLOCATE(ranking(matrixSize, 1))
 	
-	do i = 1, matrixSize
+	DO i = 1, matrixSize
 		S(i,1) = (1.0 / matrixSize) 
 		ranking(i,1) = 1.0 / matrixSize
-	end do
+	END DO
+
+	DO i = 1, matrixSize
+		DO j = 1, matrixSize
+			matrix(i,j) = (1.0-m) * matrix(i,j)  
+		END DO
+	END DO
 	
 	temp = 1
-	do while (ABS(temp -  ranking(1,1)) > threshold)
+	DO WHILE (ABS(temp -  ranking(1,1)) > threshold)
 		temp = ranking(1,1)
 		matrix = matmul(matrix, matrix)
 	    ranking = matmul(matrix, S)
-	end do
+	END DO
 
-	do i=1, matrixSize
+	DO i=1, matrixSize
 		PRINT *, ranking(i, 1)
-	end do
+	END DO
 	
-	deallocate(S)
+	DEALLOCATE(S)
+	DEALLOCATE(ranking)
 END SUBROUTINE
